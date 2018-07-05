a164c8e56848b047820981561bebc3a144640e73
arielza <ariel.zahav@gmail.com>
2018-07-05 17:25:48 +0300

    update mail sevies

---
+++ js/services/email-service.js
@@ -0,0 +1,76 @@
+import makeid from './utiles-service.js';
+var emails = [ {
+            "id": "OXeMG",
+               "subject": "unix", 
+               "body": "hi", 
+               "isRead": false, 
+               "sentAt": 1530797138},
+               {
+                "id": "JYOJa",
+                "subject": "js", 
+                "body": "hi", 
+                "isRead": false, 
+                "sentAt": 1530797128},
+                {
+                "id": "1y0Oq",
+                "subject": "css", 
+                "body": "hi", 
+                "isRead": true, 
+                "sentAt": 1530797118},
+                {
+                 "id": "JYOJa",
+                "subject": "css and html", 
+                "body": "hi", 
+                "isRead": true, 
+                "sentAt": 1530797098},
+                {
+                "id": "1y0Oq",
+                "subject": "html", 
+                "body": "hi", 
+                "isRead": true, 
+                "sentAt": 1530797088},
+            ];
+
+function emptyMail() {
+    return { 
+           "id": "",
+           "subject": "", 
+           "body": "", 
+           "isRead": false, 
+            "sentAt": 00000000
+    }
+}
+
+function query() {
+	return Promise.resolve(mails);
+}
+
+function geMailById(id) {
+	let mail = mails.find(mail => mail.id === id);
+	return Promise.resolve(book);
+}
+
+function removeMail(id) {
+	return new Promise((resolve, reject)=>{
+		setTimeout(() => {
+			var mailIdx = mails.findIndex(mail => mail.id === id)
+			mails.splice(mailIdx, 1);
+			resolve()
+		}, 2000);
+	});
+}
+
+function saveMail(mail) {
+	if (mail.id) {
+		var mailIdx = mails.findIndex(currMail => currMail.id === mail.id);
+		// Vue.js Caveat!
+		mails.splice(mailIdx, 1, book)
+		// mails[mailsIdx] = mail;
+
+	} else {
+		mail.id = makeid();
+		mails.push(mail);
+	}
+	console.log('Sevice is saving the mail', mail);
+	return Promise.resolve(mail);
+}
